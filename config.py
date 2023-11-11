220a0773d605c7e490c736029a8a301d50719b02
almanybasja <149967867+almanybasja@users.noreply.github.com>
2023-11-11 21:34:44 +0200

    Update config.py

--- config.py
+++ config.py
@@ -7,11 +7,3 @@ class Config(object):
     APP_ID = int(os.environ.get("APP_ID", 21627756))
 
     API_HASH = os.environ.get("API_HASH", "fe77fbf0cae9f7f5ece37659e2466cf1")
-
-    DEVLOO = os.environ.get("DEVLOO", "")
-    
-    MAX_ACCOUNTS = os.environ.get("MAX_ACCOUNTS", "5")
-    
-    user_bot = os.environ.get("user_bot", "")
-    
-    id_bot = os.environ.get("id_bot", "")
